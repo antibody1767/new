bef45bb8d7d29368e2f495294c51764214424306
himani <himani.soni@theggis.com>
2020-08-07 20:37:56 +0530

    hi

--- sketch.js
+++ sketch.js
@@ -1,13 +1,22 @@
 var ball;
+var dataBase, position;
+
 
 function setup(){
+    dataBase = firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    
+    var ballPositionRef = dataBase.ref("ball/position");
+    ballPositionRef.on("value",readPose,showError);
 }
 
 function draw(){
     background("white");
+    if(position!==undefined){
+
+    
     if(keyDown(LEFT_ARROW)){
         changePosition(-1,0);
     }
@@ -20,10 +29,24 @@ function draw(){
     else if(keyDown(DOWN_ARROW)){
         changePosition(0,+1);
     }
-    drawSprites();
+    drawSprites();}
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    dataBase.ref("ball/position").set({
+        x: ball.x+x,
+        y: ball.y+y
+    })
+   
+}
+
+function showError(){
+    console.log("error");
+}
+
+function readPose(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+
 }
